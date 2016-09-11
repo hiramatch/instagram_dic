db382bf7d90fdb3c027d1342b43035aa066beb67
hirama <hiramatch@gmail.com>
2016-09-11 17:48:11 +0900

    herokuへデプロイ

--- Gemfile
+++ Gemfile
@@ -41,9 +41,12 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  gem 'letter_opener_web'
 end
 
 gem 'carrierwave'
 gem 'rmagick'
 gem 'devise'
 gem 'twitter-bootstrap-rails'
+gem 'rails_12factor', group: :production
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy]
   root 'pictures#index'
 
-
+if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
